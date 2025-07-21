b2cc8f33c491649c9e4ec8b8adc01d253cee03fd
Julius <juliuskamau168@email.com>
2025-07-21 13:50:52 +0300

    fixed some logic bugs in the filter functions

--- main.js
+++ main.js
@@ -8,55 +8,80 @@ let currList = [];
 let currListCopy = [];
 
 
-function filterByLen(){
-  let binancetext = String(binanceTextHolder.innerText || "").split(" "||","||".");
+function filterByLen() {
+  let binancetext = String(binanceTextHolder.innerText || "").split(/[\s,.]+/);
   let charlen = parseInt(charLenInput.value);
   let counter = 0;
-  currList = [];
-  
-  for(i=0; i<binancetext.length; i++){
-    if(binancetext[i].length===charlen||binancetext[i].length===charlen++||binancetext[i].length===charlen--){
-      counter ++;
-      currList.push(binancetext[i]);
+  let currList = [];
+
+  for (let i = 0; i < binancetext.length; i++) {
+    const word = binancetext[i];
+    if (
+      word.length === charlen //||
+      /*word.length === charlen + 1 ||
+      word.length === charlen - 1*/
+    ) {
+      counter++;
+      currList.push(word);
     }
   }
-  alert("done.Found Words: "+counter);
-  currListHolder.innerText=currList;
-  document.getElementById('cluebtn').removeAttribute("disabled");
-  document.getElementById('multiCluebtn').removeAttribute("disabled");
+
+  alert("Done. Found Words: " + counter);
+  currListHolder.innerText = currList.join(", ");
+  document.getElementById("cluebtn").removeAttribute("disabled");
+  document.getElementById("multiCluebtn").removeAttribute("disabled");
 }
 
-function filterByClue(){
+
+function filterByClue() {
   let clueChar = clueCharInput.value;
-  let clueCharIndex = parseInt(clueCharIndexInput.value)-1;
+  let clueCharIndex = parseInt(clueCharIndexInput.value) - 1;
   let counter = 0;
-  currListCopy = [];
-  alert("true clue character index is "+clueCharIndex+" and clue character is "+clueChar);
-  
-  for(i=0; i<currList.length; i++){
-    if(currList[i][clueCharIndex] == clueChar||currList[i][clueCharIndex++] == clueChar||currList[i][clueCharIndex--]== clueChar){
-      //alert("the word is: "+currList[i]);
-      currListCopy.push(currList[i]);
+  let currListCopy = [];
+
+  alert("True clue character index is " + clueCharIndex + " and clue character is " + clueChar);
+
+  for (let i = 0; i < currList.length; i++) {
+    let word = currList[i];
+    if (
+      /*word[clueCharIndex - 1] === clueChar ||*/
+      word[clueCharIndex] === clueChar /*||
+      word[clueCharIndex + 1] === clueChar*/
+    ) {
+      currListCopy.push(word);
       counter++;
     }
-    //alert(currList[i][clueCharIndex]+" "+clueChar);
-  } 
-  //alert(clueCharIndex + " "+typeof(clueCharIndex));
-  if(counter==0){alert("no words found."); currListHolder.innerText=currList;}
-  currListHolder.innerText = currListCopy;
+  }
+
+  if (counter === 0) {
+    alert("No words found.");
+    currListHolder.innerText = currList.join(", ");
+  } else {
+    currListHolder.innerText = currListCopy.join(", ");
+  }
 }
 
-function filterByMultiClue(){
+
+function filterByMultiClue() {
   let clueChars = clueCharsInput.value;
   let counter = 0;
-  currListCopy = [];
-  for(i=0; i<currList.length; i++){
-   if(currList[i].includes(clueChars)){
-    //alert(currList[i]+" could be the word");
-    currListCopy.push(currList[i]);
-    counter ++;
-   } 
+  let currListCopy = [];
+
+  for (let i = 0; i < currList.length; i++) {
+    let word = currList[i];
+    for (let j = 0; j < clueChars.length; j++) {
+      if (word.includes(clueChars[j])) {
+        currListCopy.push(word);
+        counter++;
+        break;  // Avoid pushing the same word multiple times
+      }
+    }
   }
-  if(counter==0){alert("no words found."); currListHolder.innerText=currList;}
-  currListHolder.innerText = currListCopy;
+
+  if (counter === 0) {
+    alert("No words found.");
+    currListHolder.innerText = currList.join(", ");
+  } else {
+    currListHolder.innerText = currListCopy.join(", ");
+  }
 }
